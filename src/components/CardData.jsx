14408411f93bea29a18a9d543d15dcee410c88f1
Indra Setyajiwirawan Guritno <indrasetyajiwirawanguritno@gmail.com>
2020-04-11 22:39:32 +0700

    Make card with data

---
+++ src/components/CardData.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+
+const CardData = (props) => {
+  return (
+    <Container>
+      <Row>
+        <Col md={6} lg={4}>
+          <Card className="card-data">
+            <Card.Header as="h3">Cases</Card.Header>
+            <Card.Body>
+              <Card.Text as="h4">{props.data.cases}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6} lg={4}>
+          <Card className="card-data">
+            <Card.Header as="h3">Recovered</Card.Header>
+            <Card.Body>
+              <Card.Text as="h4">{props.data.recovered}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={12} lg={4}>
+          <Card className="card-data">
+            <Card.Header as="h3">Deaths</Card.Header>
+            <Card.Body>
+              <Card.Text as="h4">{props.data.deaths}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default CardData;
