5f413455e73170e17c5ab6efda7770c0d1e09278
Indra Setyajiwirawan Guritno <indrasetyajiwirawanguritno@gmail.com>
2020-04-11 22:39:11 +0700

    adding Title

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <section id="title">
+      <div>
+        <h1>Novel Coronavirus (COVID-19)</h1>
+        <p>Stay Safe, Stay Healty, Stay Home</p>
+      </div>
+    </section>
+  );
+};
+
+export default Title;
